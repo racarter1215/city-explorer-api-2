ca3f0b94dc9cda8bb5fa772db805a605b4555d90
racarter1215 <racarter1215@gmail.com>
2020-06-11 13:32:50 -0700

    first bit of lab 8

--- server.js
+++ server.js
@@ -39,14 +39,29 @@ function Trails(trail) {
 app.get('/location', (request, response) => {
     const city = request.query.city;
     const locationUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`;
-    superagent.get(locationUrl) 
-        .then(finalLocationStuff => {
-            const location = new Location(city, finalLocationStuff.body[0]);
-            response.send(location);
+    let sql =   `SELECT * FROM locations WHERE search_query=$1;`;
+    let searchValues = [city];
+
+    dbClient.query(sql, searchValues)
+        .then(store => {
+            if (store.rows[0]) {
+                response.status(200).send(record.rows[0]);
+            } else {
+                superagent.get(locationUrl) 
+                    .then(finalLocationStuff => {
+                        let location = new Location(city, finalLocationStuff.body[0]);
+                        let sqlAdd = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);`;
+                        let searchValues = [cityQuery, location.formatted_query, location.latitude, location.longitude]
+                        dbClient.query(sqlAdd, searchValues);
+                        response.status(200).send(location);
+                }).catch(error => {
+                errorHandler(error, request, response);
+                });
+            }
         }).catch(error => {
-        errorHandler(error, request, response);
-    });
-});
+            errorHandler(error, request, response);
+            });
+        });
 
 app.get('/weather', (request, response) => {
     const { latitude, longitude} = request.query;
