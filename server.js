f52d0b4f7ecb1b065b313f1f1b869d655517bd3a
racarter1215 <racarter1215@gmail.com>
2020-06-12 14:36:13 -0700

    finished lab 9

--- server.js
+++ server.js
@@ -46,13 +46,14 @@ function Yelp(obj) {
 }
 
 function Movie(obj) {
+    console.log("constructor", obj);
     this.title = obj.title;
     this.overview = obj.overview;
     this.average_votes = obj.vote_average;
     this.total_votes = obj.vote_count;
-    this.image_url = `https://image.tmdb.org/t/p/w500${obj.backdrop_path}`;
+    this.image_url = `https://image.tmdb.org/t/p/original${obj.poster_path}`;
     this.popularity = obj.popularity;
-    this.released_on = obj.release.date;
+    this.released_on = obj.release_date;
 }
 
 app.get('/location', (request, response) => {
@@ -140,14 +141,18 @@ app.get('/movies', (request, response) => {
     let city = request.query.search_query;
     let key = process.env.MOVIE_API_KEY;
     let moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${city}`;
-
+    // console.log(moviesUrl)
     superagent.get(moviesUrl)
         .then(movieResponse => {
+            // console.log(movieResponse.body)
             const movieData = movieResponse.body.results;
-            response.send(movieData.map(data => {
+            // console.log("movie data!!!!!!", movieData);
+            let mappedData = movieData.map(data => {
+                console.log("FIRST DATA", data);
                 return new Movie(data);
-            }));
-            console.log(data);
+            });
+            console.log(mappedData);
+            response.send(mappedData);
         }).catch(error => errorHandler(error, request, response));
 }); 
 
